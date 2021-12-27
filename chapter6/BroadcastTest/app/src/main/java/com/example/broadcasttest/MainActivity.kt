150121636e77d6f3410dc932e2bc4b5108f0955d
dingziqi <dingziqi_xtu@sina.com>
2021-12-27 16:10:36 +0800

    case: 动态注册广播接口器

--- chapter6/BroadcastTest/app/src/main/java/com/example/broadcasttest/MainActivity.kt
+++ chapter6/BroadcastTest/app/src/main/java/com/example/broadcasttest/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.example.broadcasttest
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.ResultReceiver
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var timeChangeReceiver: TimeChangeReceiver
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val intentFilter = IntentFilter()
+        intentFilter.addAction("android.intent.action.TIME_TICK")
+
+        timeChangeReceiver = TimeChangeReceiver()
+        registerReceiver(timeChangeReceiver, intentFilter)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        unregisterReceiver(timeChangeReceiver)
+    }
+
+    inner class TimeChangeReceiver: BroadcastReceiver() {
+        override fun onReceive(p0: Context?, p1: Intent?) {
+            Toast.makeText(p0, "Time has Changed", Toast.LENGTH_LONG).show()
+        }
     }
 }
