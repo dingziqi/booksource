3fd24f8750bb7ea7f84131c0c459f9b8305c35d8
dingziqi <dingziqi_xtu@sina.com>
2021-12-28 19:46:41 +0800

    case: File 存储

--- chapter7/FilePersistenceTest/app/src/main/java/com/example/filepersistencetest/MainActivity.kt
+++ chapter7/FilePersistenceTest/app/src/main/java/com/example/filepersistencetest/MainActivity.kt
@@ -1,11 +1,54 @@
 package com.example.filepersistencetest
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.*
+import java.lang.StringBuilder
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val cache = load()
+        text_edit.setText(cache.toString())
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        save(text_edit.text.toString())
+    }
+
+    private fun save(inputString: String) {
+        try {
+            val output = openFileOutput("data", Context.MODE_PRIVATE)
+            val writer = BufferedWriter(OutputStreamWriter(output))
+
+            writer.use {
+                it.write(inputString)
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun load(): String {
+        val content = StringBuilder()
+        try{
+            val input = openFileInput("data")
+            val reader = BufferedReader(InputStreamReader(input))
+
+            reader.use{
+                it.forEachLine {
+                    content.append(it)
+                }
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+        return content.toString()
     }
 }
